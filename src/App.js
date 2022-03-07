610e8abbcd0dbd9dec70c986781ae649acbce75e
Le Hoang Trong <langtupro0456@gmail.com>
2022-03-07 13:39:35 +0700

    Learn Config Theme in JSS

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import UserProfile from './FormValidation/UserProfie/UserProfile'
 import DemoJSS from './JSS_StyledComponents/DemoJSS/DemoJSS';
+import DemoTheme from './JSS_StyledComponents/Themes/DemoTheme';
+
 
 function App() {
   return (
+
+    // ******************************* Demo về Theme
+    <div>
+      <DemoTheme />
+    </div>
     
     // ******************************* Demo về JSS
-    <div>
-      <DemoJSS />
-    </div>
+    // <div>
+    //   <DemoJSS />
+    // </div>
 
     // ******************************** DemoProfile về Form Validation
     // <div> 
